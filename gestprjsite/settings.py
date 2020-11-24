80f5136f7ae36b415cce6bf1b94bc31716900d05
dani <d.martinez@creaf.uab.cat>
2020-11-24 10:50:04 +0100

    SecurityMiddleware quitado por problemas de compatibilidad con versión de Django inferior a 1.8

--- gestprjsite/settings.py
+++ gestprjsite/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+    #'django.middleware.security.SecurityMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
